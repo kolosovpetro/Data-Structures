54b7a5f5024cc64f4993efc902d3ff5ce4ef782b
razumovskiy <kolosovp94@gmail.com>
2020-09-22 12:12:59 +0300

    project is beaing initialized

---
+++ DataStructures.Stack/Abstractions/IStack.cs
@@ -0,0 +1,11 @@
+﻿namespace DataStructures.Stack.Abstractions
+{
+    public interface IStack<T>
+    {
+        int Count { get; }
+        bool IsEmpty { get; }
+        void Push(T entity);
+        T Pop();
+        T Peek();
+    }
+}
---
+++ DataStructures.Stack/Concrete/StackViaArray.cs
@@ -0,0 +1,24 @@
+﻿using DataStructures.Stack.Abstractions;
+
+namespace DataStructures.Stack.Concrete
+{
+    public class StackViaArray<T> : IStack<T>
+    {
+        public int Count { get; }
+        public bool IsEmpty { get; }
+        public void Push(T entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T Pop()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public T Peek()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

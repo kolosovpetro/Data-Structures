487eae950b1cd91fdf88c92dfa11307a8addb82b
petro kolosov <kolosovp94@gmail.com>
2020-09-24 15:27:17 +0300

    double linked list initialized

---
+++ DataStructures.LinkedList/Concrete/DoubleLinkedList.cs
@@ -0,0 +1,52 @@
+﻿using DataStructures.LinkedList.Abstractions;
+
+namespace DataStructures.LinkedList.Concrete
+{
+    public class DoubleLinkedList<T> : ILinkedList<T>
+    {
+        public bool IsEmpty { get; }
+        public int Count { get; }
+        public ILinkedListNode<T> First { get; }
+        public ILinkedListNode<T> Last { get; }
+        
+        public ILinkedListNode<T> AddFirst(T entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemoveFirst()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ILinkedListNode<T> AddLast(T entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemoveLast()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Contains(T entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ILinkedListNode<T> Find(T entity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Reverse()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
